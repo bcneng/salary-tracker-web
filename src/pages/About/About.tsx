d7787f237e66f728fe1c394146750d6d7f3e244a
Manuel Obregozo <obregozo.manuel@gmail.com>
2020-04-11 15:59:29 +0200

    fix: remove old demo code

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -21,24 +21,7 @@ const About: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <IonGrid className="page-header" fixed>
-          <IonCard>
-            <img
-              alt="barcelona"
-              style={{ height: '150px', objectFit: 'cover', width: '100%' }}
-              src="https://www.eindhovenairport.nl/sites/default/files/styles/header_image_bestemming/public/barcelona_1.jpg?itok=UqE05-BM&c=eb5fe87ae2d5ccb93351e16083d7b920"
-            />
-            <IonCardHeader>
-              <IonCardTitle>Salary Tracker</IonCardTitle>
-            </IonCardHeader>
-            <IonCardContent>
-              Salary Tracker is an application Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Quos, officiis! Natus iusto, itaque
-              consequuntur sed aliquam beatae, rem a impedit quia eum libero
-              facilis dolorum, fugiat dolore accusantium architecto amet.
-            </IonCardContent>
-          </IonCard>
-        </IonGrid>
+        <IonGrid className="page-header" fixed></IonGrid>
       </IonContent>
     </IonPage>
   );
